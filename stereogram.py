04d8dd7a23b70051424c71788eacc02724781267
Julien NOEL <juliennoel9@gmail.com>
2021-06-04 12:17:59 +0200

    Update

--- stereogram.py
+++ stereogram.py
@@ -108,7 +108,6 @@ def pointFixe(a, b, c, d, nbY, nbPts, f, e):
         xyP = []
         # Generation des pts
         for j in range(nbPts):
-            print(j)
             xyP.append([randint(a, b), c + ((d - c) / nbY) * k])
             xyI = [calculTd(xyP[j][0], xyP[j][1], calculZ2(xyP[j][0], xyP[j][1]), f, e)]
             nbI = 1
@@ -118,8 +117,7 @@ def pointFixe(a, b, c, d, nbY, nbPts, f, e):
                 xp = calculXp(xyI[nbI - 1][0], sAppro, e)
                 xyI.append(calculTd(xp, c + ((d - c) / nbY) * k, calculZ(xp, c + ((d - c) / nbY) * k), f, e))
                 nbI = nbI + 1
-                print(xyI)
-            plt.plot(xyI, '.', markersize=20)
+            plt.plot(np.array(xyI)[:, 0], np.array(xyI)[:, 1], '.', markersize=2, color='black')
     plt.show()
 
 
@@ -137,18 +135,6 @@ def approxDirect(a, b, c, d, nbY, nbPts, f, e, markersize):
                 xyI.append([xyI[nbI - 1][0] + sAppro, c + ((d - c) / nbY) * k])
                 nbI = nbI + 1
             plt.plot(np.array(xyI)[:, 0], np.array(xyI)[:, 1], '.', markersize=markersize,color='black')
-    plt.show()
-
-
-def test(nbPoints, f, e):
-    transforme = []
-    for i in range(0, nbPoints):
-        x = randint(0, 6)
-        y = randint(-10, 10)
-        z = calculZ2(x, y)
-        transforme.append([(f * x) / z, (f * y) / z])
-        transforme.append([((f * (x - e)) / z) + e, (f * y) / z])
-    plt.plot(np.array(transforme)[:,0],np.array(transforme)[:,1], '.', markersize=5, color='black')
     plt.show()
 
 
@@ -191,19 +177,19 @@ if __name__ == '__main__':
     # Tetraedre = [P1,P2,P3,P4]
     tetraedre = np.array([[1.2, -0.5, 65], [3, 1.5, 63], [4, -1, 62], [1.8, -1.5, 62]])
     # Figure n°1 rapport
-    # affichage(tetraedre)
+    affichage(tetraedre)
     # Figure n°5 rapport
-    # affichage_transformations(tetraedre)
+    affichage_transformations(tetraedre)
 
     # Prisme = [P1,P2,P3,P4,P5,P6]
     prisme = np.array([[1, -1, 65], [1.8, -2, 62], [4.2, -1.5, 65], [1, 2, 65], [1.8, 1, 62], [4.2, 1.5, 65]])
     prisme_aretes = np.array([[0, 1], [0, 2], [0, 3], [1, 2], [1, 4], [2, 5], [3, 4], [3, 5], [4, 5]])
     # Figure n°2 rapport
-    # affichage(prisme)
+    affichage(prisme)
     # Figure n°3 rapport
-    # affichage(prisme, prisme_aretes)
+    affichage(prisme, prisme_aretes)
     # Figure n°6 rapport
-    # affichage_transformations(prisme, prisme_aretes)
+    affichage_transformations(prisme, prisme_aretes)
 
     # Polyedre
     z = 64
@@ -211,23 +197,23 @@ if __name__ == '__main__':
                          [0, 2, z + 1], [0, 3, z + 1], [3, 3, z + 1], [3, 2, z + 1], [2, 2, z + 1], [2, 0, z + 1],
                          [1, 0, z + 1], [1, 2, z + 1]])
     # Figure n°4 rapport
-    # affichage(polyedre, calcul_aretes_polyedre_regulier(polyedre, 8))
+    affichage(polyedre, calcul_aretes_polyedre_regulier(polyedre, 8))
     # Figure n°7 rapport
-    # affichage_transformations(polyedre, calcul_aretes_polyedre_regulier(polyedre, 8))
+    affichage_transformations(polyedre, calcul_aretes_polyedre_regulier(polyedre, 8))
 
     # Carré
     carre = np.array([[0,0,z],[0,4,z],[4,4,z],[4,0,z],[0,0,z+4],[0,4,z+4],[4,4,z+4],[4,0,z+4]])
     # Figure n°8 rapport
-    # affichage(carre, calcul_aretes_polyedre_regulier(carre, 4))
+    affichage(carre, calcul_aretes_polyedre_regulier(carre, 4))
     # Figure n°9 rapport
-    # affichage_transformations(carre, calcul_aretes_polyedre_regulier(carre, 4))
+    affichage_transformations(carre, calcul_aretes_polyedre_regulier(carre, 4))
 
     # Pentagone
     pentagone = np.array([[1,0,z],[0.309,0.951,z],[-0.809,0.588,z],[-0.809,-0.588,z],[0.309,-0.951,z],[1,0,z+1],[0.309,0.951,z+1],[-0.809,0.588,z+1],[-0.809,-0.588,z+1],[0.309,-0.951,z+1]])
     # Figure n°10 rapport
-    # affichage(pentagone, calcul_aretes_polyedre_regulier(pentagone,5))
+    affichage(pentagone, calcul_aretes_polyedre_regulier(pentagone,5))
     # Figure n°11 rapport
-    # affichage_transformations(pentagone, calcul_aretes_polyedre_regulier(pentagone,5))
+    affichage_transformations(pentagone, calcul_aretes_polyedre_regulier(pentagone,5))
 
     # Figure 12
     ptsAleaSurface(800,40,7)
@@ -241,3 +227,5 @@ if __name__ == '__main__':
     approxDirect(-20, 25, -11, 20, 150, 50, 12, 7, 3)
     # Figure 17
     approxDirect(-20, 25, -11, 20, 150, 50, 12, 7, 1)
+    # Figure 18
+    pointFixe(-20, 25, -11, 20, 150, 50, 12, 7)
